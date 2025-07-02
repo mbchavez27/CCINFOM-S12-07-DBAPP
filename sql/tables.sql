a846568695fb236584b455303d75caa7d5c1ebd0
givemeanameidk <paulplacermain@gmail.com>
2025-07-02 16:34:11 +0800

    Update tables.sql
    Fixed syntax errors for table creation queries

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,70 +1,72 @@
-create database laptop_borrowing;
+CREATE DATABASE laptop_borrowing;
+USE laptop_borrowing;
 
-create table laptops (
-	laptop_id decimal(8,0) not null auto_increment,
-	product_name varchar(255) not null,
+CREATE TABLE laptops (
+	laptop_id int NOT NULL AUTO_INCREMENT,
+	product_name varchar(255) NOT NULL,
 	product_os varchar(50),
-	battery_health int not null,
-	constraint laptop_pk primary key (laptop_id),
-	constraint chk_battery check (battery_health between 1 and 100),
-	constraint chk_os check (product_os in ('Windows', 'MacOS', 'Linux'))
+	battery_health int NOT NULL,
+	CONSTRAINT laptop_pk PRIMARY KEY(laptop_id),
+	CONSTRAINT chk_battery CHECK(battery_health between 1 and 100),
+	CONSTRAINT chk_os CHECK(product_os in ('Windows', 'MacOS', 'Linux'))
 );
 
-create table customers (
-	customer_id decimal(8,0) not null auto_increment,
-	last_name varchar(255) not null,
-	first_name varchar(255) not null,
-	type varchar(50) not null,
-	college varchar(50) not null,
-	constraint customer_pk primary key (customer_id),
-	constraint chk_type check (type in ('Student', 'Faculty')),
-	constraint chk_college check (college in ('College of Business', 'College of Education', 
+CREATE TABLE customers (
+	customer_id int NOT NULL AUTO_INCREMENT,
+	last_name varchar(255) NOT NULL,
+	first_name varchar(255) NOT NULL,
+	type varchar(50) NOT NULL,
+	college varchar(50) NOT NULL,
+	CONSTRAINT customer_pk PRIMARY KEY(customer_id),
+	CONSTRAINT chk_type CHECK(type IN ('Student', 'Faculty')),
+	CONSTRAINT chk_college CHECK(college IN ('College of Business', 'College of Education', 
 						'School of Law', 'College of Engineering',
 						'College of Liberal Arts', 'College of Science',
 						'College of Computer Studies', 'School of Economics'))
 );
 
-create table staffs (
-	staff_id decimal(8,0) not null auto_increment,
-	last_name varchar(255) not null,
-	first_name varchar(255) not null,
+CREATE TABLE staff (
+	staff_id int NOT NULL AUTO_INCREMENT,
+	last_name varchar(255) NOT NULL,
+	first_name varchar(255) NOT NULL,
 	role varchar(50),
-	contact varchar(20) not null,
-	constraint staff_pk primary key (staff_id)
+	contact varchar(20) NOT NULL,
+	constraint staff_pk PRIMARY KEY(staff_id)
 );
 
-create table tickets (
-	ticket_id decimal(8,0) not null auto_increment,
-	laptop_id decimal(8,0) not null,
-	staff_id decimal(8,0) not null,
-	customer_id decimal(8,0) not null,
-	issue_type varchar(50) not null,
-	description varchar(255) not null,
-	date_opened date not null,
+CREATE TABLE tickets (
+	ticket_id int NOT NULL AUTO_INCREMENT,
+	laptop_id int NOT NULL,
+	staff_id int NOT NULL,
+	customer_id int NOT NULL,
+	issue_type varchar(50) NOT NULL,
+	description varchar(255) NOT NULL,
+	date_opened date NOT NULL,
 	date_closed date,
-	status varchar(10) as (
-        case
-            when date_closed is null then 'open'
-            else 'closed'
-        end
-    ) stored,
-	constraint chk_issue_type check (issue_type in ('Software', 'Hardware', 'Other')),
-	constraint chk_date_order check (date_closed is null or date_opened < date_closed),
-	constraint ticket_pk primary key (ticket_id),
-	constraint laptop_fk foreign key (laptop_id) references laptops(laptop_id),
-	constraint staff_fk foreign key (staff_id) references staffs(staff_id),
-	constraint customer_fk foreign key (customer_id) references customers(customer_id)
+	status varchar(10) AS (
+        CASE
+            WHEN date_closed IS NULL THEN 'open'
+            ELSE 'closed'
+        END
+    ) STORED,
+	CONSTRAINT chk_issue_type CHECK(issue_type IN ('Software', 'Hardware', 'Other')),
+	CONSTRAINT chk_date_closed CHECK(date_closed IS NULL OR date_closed >= date_opened),
+	CONSTRAINT ticket_pk PRIMARY KEY(ticket_id),
+	CONSTRAINT ticket_fk1 FOREIGN KEY(laptop_id) REFERENCES laptops(laptop_id),
+	CONSTRAINT ticket_fk2 FOREIGN KEY(staff_id) REFERENCES staff(staff_id),
+	CONSTRAINT ticket_fk3 FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
 
-create table borrow_record (
-	borrow_id decimal(8,0) not null auto_increment,
-	laptop_id decimal(8,0) not null,
-	staff_id decimal(8,0) not null,
-	customer_id decimal(8,0) not null,
-	pickup_date date not null,
+create table borrow_records (
+	borrow_id int NOT NULL AUTO_INCREMENT,
+	laptop_id int NOT NULL,
+	staff_id int NOT NULL,
+	customer_id int NOT NULL,
+	pickup_date date NOT NULL,
 	return_date date,
-	constraint chk_date check (return_date is null or pickup_date < return_date),
-	constraint laptop_fk foreign key (laptop_id) references laptops(laptop_id),
-	constraint staff_fk foreign key (staff_id) references staffs(staff_id),
-	constraint customer_fk foreign key (customer_id) references customers(customer_id)
+	CONSTRAINT chk_return_date CHECK(return_date IS NULL OR return_date >= pickup_date),
+    CONSTRAINT borrow_record_pk PRIMARY KEY(borrow_id),
+	CONSTRAINT borrow_record_fk1 FOREIGN KEY(laptop_id) REFERENCES laptops(laptop_id),
+	CONSTRAINT borrow_record_fk2 FOREIGN KEY(staff_id) REFERENCES staff(staff_id),
+	CONSTRAINT borrow_record_fk3 FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
