7a00b5b2c8abe9fbc9735b1d78aba1a493eed63f
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-07-10 11:29:30 +0900

    feat: add customers.sql

---
+++ sql/customers.sql
@@ -0,0 +1,5 @@
+#SQL Script
+INSERT INTO customers (last_name, first_name, type, college_id) values (?, ?, ?, ?);
+
+#Sample Input
+INSERT INTO customers (last_name, first_name, type, college_id) values ('Zarate', 'Julia', 'Student', 'CCS');
