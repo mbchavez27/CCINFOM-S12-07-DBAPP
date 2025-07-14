d6c6032f5f16e5ee8391168f57c11c7840985712
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-07-14 11:44:43 +0800

    feat: added delete laptops

--- server/controller/laptops.controller.js
+++ server/controller/laptops.controller.js
@@ -19,3 +19,35 @@ export const getLaptops = async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' })
   }
 }
+
+export const addLaptops = async (req, res) => {
+  const { product_name, product_os, battery_health } = req.body
+
+  try {
+    const newLaptop = await laptopsService.addLaptops(
+      product_name,
+      product_os,
+      battery_health
+    )
+
+    res.status(201).json(newLaptop)
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
+
+export const deleteLaptops = async (req, res) => {
+  const { laptop_id } = req.body
+
+  try {
+    const success = await laptopsService.deleteLaptops(laptop_id)
+
+    if (success) {
+      res.status(200).json({ message: 'Laptop deleted successfully' })
+    } else {
+      res.status(404).json({ message: 'Laptop not found' })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' })
+  }
+}
--- server/routes/laptops.routes.js
+++ server/routes/laptops.routes.js
@@ -3,5 +3,7 @@ import * as laptopController from '../controller/laptops.controller.js'
 const router = express.Router()
 
 router.get('/', laptopController.getLaptops)
+router.get('/add', laptopController.addLaptops)
+router.get('/delete', laptopController.deleteLaptops)
 
 export default router
