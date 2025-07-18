329a3487928a8b6e4acd7bf05cae3da3b4152ced
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-07-19 05:21:55 +0800

    feat: finished borrow history

--- client/src/components/CustomerDashboard.jsx
+++ client/src/components/CustomerDashboard.jsx
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
-import StatusCard from "./StatusCard";
-import { getCustomerCurrentlyBorrowing } from "../services/customer.services.js";
-import { getCurrentPenalty } from "../services/penalty.services.js";
+import { useEffect, useState } from 'react'
+import StatusCard from './StatusCard'
+import { getCustomerCurrentlyBorrowing } from '../services/customer.services.js'
+import { getCurrentPenalty } from '../services/penalty.services.js'
 
 function CustomerDashboard({ customer_id }) {
-  const [loading, setLoading] = useState(false);
-  const [current, setCurrent] = useState({});
+  const [loading, setLoading] = useState(false)
+  const [current, setCurrent] = useState({})
   const fetchCurrent = async () => {
-    setLoading(false);
-    const res = await getCustomerCurrentlyBorrowing(customer_id);
-    setCurrent(res.data.data);
-    setLoading(true);
-  };
+    setLoading(false)
+    const res = await getCustomerCurrentlyBorrowing(customer_id)
+    setCurrent(res.data.data)
+    setLoading(true)
+  }
 
   useEffect(() => {
-    fetchCurrent();
-  }, []);
+    fetchCurrent()
+  }, [])
 
-  const [penaltyLoading, setPenaltyLoading] = useState(false);
-  const [penalty, setPenalty] = useState({});
+  const [penaltyLoading, setPenaltyLoading] = useState(false)
+  const [penalty, setPenalty] = useState({})
 
   const fetchPenalty = async () => {
-    setPenaltyLoading(false);
-    const res = await getCurrentPenalty(customer_id);
-    setPenalty(res.data.data);
-    setPenaltyLoading(true);
-  };
+    setPenaltyLoading(false)
+    const res = await getCurrentPenalty(customer_id)
+    setPenalty(res.data.data)
+    setPenaltyLoading(true)
+  }
 
   useEffect(() => {
-    fetchPenalty();
-  }, []);
+    fetchPenalty()
+  }, [])
 
   return (
     <div className="flex flex-col p-17.5">
@@ -48,20 +48,40 @@ function CustomerDashboard({ customer_id }) {
                   7 * 24 * 60 * 60 * 1000
               )
                 .toISOString()
-                .split("T")[0]
+                .split('T')[0]
             }`}
           />
-        ) : null}
+        ) : (
+          <StatusCard
+            header="No Laptops Found"
+            text="Not Borrowing at the Moment"
+            subtext="Borrow laptop now..."
+          />
+        )}
         {penaltyLoading ? (
           <StatusCard
             header="Penalty Imposed"
-            text="Status"
+            text={`Date Imposed: ${new Date(
+              penalty.date_imposed
+            ).toLocaleDateString('en-US', {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            })}`}
             subtext="Cannot Borrow till Settled"
           />
-        ) : null}
+        ) : (
+          <StatusCard
+            header="No Penalty Imposed"
+            text="No Penalties at the Moment"
+            subtext={`${
+              current.product_name == null ? 'Can' : 'Cannot'
+            } borrow a laptop`}
+          />
+        )}
       </div>
     </div>
-  );
+  )
 }
 
-export default CustomerDashboard;
+export default CustomerDashboard
